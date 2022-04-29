68732af69e776f4cbe93986b29716c2bf2c1905a
zpqmdh <87455115+zpqmdh@users.noreply.github.com>
2022-04-29 23:50:18 +0900

    solved

--- dfs-bfs/2667.py
+++ dfs-bfs/2667.py
@@ -8,16 +8,19 @@ def dfs(y, x):
         nx = x + dx[i]
         if is_possible(ny,nx) == True and square[ny][nx] == '1' and visited[ny][nx] == False:
             dfs(ny, nx)
+            
 def is_possible(y, x):
     if y<0 or y>=N or x<0 or x>=N:
         return False
     return True
+
 dy = [1, -1, 0, 0]
 dx = [0, 0, 1, -1]
 N = int(input())
 square = []
 visited = [[False] * N for _ in range(N)]
 house = []
+
 for i in range(N):
     square.append(input())
 
@@ -27,6 +30,7 @@ for i in range(N):
             cnt = 0
             dfs(i, j)
             house.append(cnt)
+
 print(len(house))
 for i in sorted(house):
     print(i)
