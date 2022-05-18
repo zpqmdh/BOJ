f1ee9575ef73a8e20a40e9de077f0fd9a87345f8
zpqmdh <87455115+zpqmdh@users.noreply.github.com>
2022-05-18 23:27:36 +0900

    solved

---
+++ dynamic_programming/10826.py
@@ -0,0 +1,14 @@
+# 피보나치 수
+def fibonacci(N):
+    if N >= 1: 
+        dp[1] = 1
+    if N >= 2:
+        dp[2] = 1
+
+    for i in range(3, N+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[N]
+N = int(input())
+dp = [0] * (N+1)
+print(fibonacci(N))
+print(dp)
