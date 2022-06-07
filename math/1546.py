4cd19a61630173b80a532a62bbbb1f680722d172
zpqmdh <87455115+zpqmdh@users.noreply.github.com>
2022-06-07 23:21:16 +0900

    solved

---
+++ math/1546.py
@@ -0,0 +1,9 @@
+# 평균
+import sys
+input = sys.stdin.readline
+N = int(input())
+score = list(map(int, input().split()))
+M = max(score)
+for i in range(N):
+    score[i] = score[i] / M * 100
+print(sum(score) / N)
