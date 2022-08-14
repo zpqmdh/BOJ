5ae235da331064414be6d1e4fd6e49732b7bf0ed
zpqmdh <ysys1130@gmail.com>
2022-08-14 15:30:44 +0900

    solved

--- dynamic_programming/12865.py
+++ dynamic_programming/12865.py
@@ -1,10 +1,17 @@
-# DP
+# 평범한 배낭
 import sys
-N, K = map(int, sys.stdin.readline().split())
-arr = []
-for i in range(N):
-    w, v = map(int, sys.stdin.readline().split())
-    arr.append([w, v])
-weight = 0
-while weight <= K:
-    
+input = sys.stdin.readline
+N, K = map(int, input().split())
+bag = [[0, 0]]
+dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+for _ in range(N):
+    bag.append(list(map(int, input().split())))
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        weight = bag[i][0]
+        value = bag[i][1]
+        if j < weight: 
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+print(dp[N][K])
