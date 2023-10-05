aff7388be5d224af60921304ad143f4aa1495c4d
zpqmdh <ysys1130@gmail.com>
2023-10-05 12:59:05 +0900

    solved

--- tree/1991.py
+++ tree/1991.py
@@ -3,16 +3,47 @@ import sys
 input = sys.stdin.readline
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, left, right):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
-class BinaryTree():
-    def __init__(self):
-        self.root = None
+# 전위 순회
+def pre_order(node):
+    print(node.data, end='')
+    if node.left != None:
+        pre_order(tree[node.left])
+    if node.right != None:
+        pre_order(tree[node.right])
+
+# 중위 순회
+def in_order(node):
+    if node.left != None:
+        in_order(tree[node.left])
+    print(node.data, end='')
+    if node.right != None:
+        in_order(tree[node.right])
+
+# 후위 순회
+def post_order(node):
+    if node.left != None:
+        post_order(tree[node.left])
+    if node.right != None:
+        post_order(tree[node.right])
+    print(node.data, end='')
 
 N = int(input())
-tree = BinaryTree()
+tree = {}
 for _ in range(N):
-    
+    data, left, right = input().split()
+    if left == '.':
+        left = None
+    if right == '.':
+        right = None
+    tree[data] = Node(data, left, right)
+
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
