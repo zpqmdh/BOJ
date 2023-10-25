2a0e96ba9f37f5c01011a9bf870d0d6edff34282
zpqmdh <ysys1130@gmail.com>
2023-10-25 18:53:14 +0900

    solved

---
+++ data_structures/28278.py
@@ -0,0 +1,22 @@
+# 스택 2
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+for _ in range(N):
+    cmd = list(map(int, input().split()))
+    if cmd[0] == 1: stack.append(int(cmd[1]))
+    elif cmd[0] == 2:
+        if stack: 
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 3:
+        print(len(stack))
+    elif cmd[0] == 4:
+        if stack: print(0)
+        else: print(1)
+    elif cmd[0] == 5:
+        if stack: print(stack[-1])
+        else: print(-1)
