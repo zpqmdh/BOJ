316454261a113b50d1f9990758f0ebdb5ce110a3
zpqmdh <ysys1130@gmail.com>
2023-11-05 14:58:36 +0900

    add another solution

--- data_structures/1021.py
+++ data_structures/1021.py
@@ -1,4 +1,6 @@
 # 회전하는 큐
+
+# 풀이 1
 import sys
 input = sys.stdin.readline
 from collections import deque
@@ -27,3 +29,31 @@ while True:
             else:
                 Q.appendleft(Q.pop()) # 3
                 cnt += 1
+
+# 풀이 2
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+Q = deque([i for i in range(1, N+1)])
+res = []
+idx = 0
+cnt = 0
+while True:
+    if num == res:
+        print(cnt)
+        break
+    cur = Q.popleft() # 1번 연산
+    if num[idx] == cur:
+        res.append(cur)
+        idx += 1
+    else:
+        Q.appendleft(cur) # 1번 연산 원래대로
+        if Q.index(num[idx]) < len(Q)/2: # 찾고자 하는 값이 큐의 앞쪽에 있을 때 -> 2번 연산이 효과적
+            Q.append(Q.popleft())
+            cnt += 1
+        else: # 찾고자 하는 값이 큐의 뒤쪽에 있을 때 -> 3번 연산이 효과적
+            Q.appendleft(Q.pop())
+            cnt += 1
