98111ec67e608d23a3b7b580682c02aea533645f
zpqmdh <ysys1130@gmail.com>
2023-12-31 20:44:24 +0900

    solved

--- bruteforce/15686.py
+++ bruteforce/15686.py
@@ -3,37 +3,37 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-city = list(list(map(int, input().split())) for _ in range(N))
+city = [list(map(int, input().split())) for _ in range(N)]
 home = []
 chicken = []
 selected_chicken = []
 answer = 99999
 
-def solve(cnt):
+def solve(idx, cnt):
     if cnt == M:
         check_distance()
         return
-    for i in range(len(chicken)):
-        if i in selected_chicken:
-            continue
+    for i in range(idx, len(chicken)):
         selected_chicken.append(i)
-        solve(cnt+1)
+        solve(i+1, cnt+1)
         selected_chicken.pop()
 
 def check_distance():
     global answer
-    distance = [987654321 for _ in range(len(home))]
+    result = 0
     for i in range(len(home)):
+        distance = 1e5
         for j in selected_chicken:
-            distance[i] = min(distance[i], abs(home[i][0]-chicken[j][0])+abs(home[i][1]-chicken[j][1]))
-    answer = min(answer, sum(distance))
+            distance = min(distance, abs(home[i][0]-chicken[j][0])+abs(home[i][1]-chicken[j][1]))
+        result += distance
+    answer = min(answer, result)
 
 for i in range(N):
     for j in range(N):
-        if city[i][j] == 1:
+        if city[i][j] == 1: # 집
             home.append([i,j])
-        if city[i][j] == 2:
+        if city[i][j] == 2: # 치킨집
             chicken.append([i,j])
 
-solve(0)
+solve(0, 0)
 print(answer)
